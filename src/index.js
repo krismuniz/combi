afc6175d1d891910de70b3aa5d0d300c8c82b3d0
Kristian Muñiz <kristianmunix@gmail.com>
2018-11-17 22:11:27 -0400

    Shave off some bytes by simplifying code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 export default function (cb, preventDefault = false) {
-  let KEY_REGEX = /Key|Digit/
-  let MODS = ['shift', 'meta', 'control', 'option', 'alt']
-
   return (e) => {
     if (!e) return false
-    if (preventDefault !== false) e.preventDefault()
+    if (preventDefault) e.preventDefault()
 
-    let keys = [
+    let combo = [
       e.ctrlKey ? 'ctrl' : undefined,
       e.altKey ? 'alt' : undefined,
       e.shiftKey ? 'shift' : undefined,
       e.metaKey ? 'meta' : undefined,
-      MODS.includes(e.key.toLowerCase()) ? undefined : e.code.replace(KEY_REGEX, '')
-    ]
-
-    let combo = keys.filter(Boolean)
+      ['Shift', 'Meta', 'Control', 'Option', 'Alt'].includes(e.key) ? undefined : e.code.replace(/Key|Digit/, '')
+    ].filter(Boolean)
 
     if (combo.length >= 2) {
       if (cb) {
